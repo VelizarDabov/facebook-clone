36c060c44c2f0a1f19f323f672dc246b8e77200e
VelizarDabov <velizardabov@gmail.com>
2024-01-09 11:57:13 +0200

    setup

---
+++ components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className=''>
+        {/* header left  */}
+        <div>
+
+        </div>
+        {/* hader center */}
+        <div>
+            
+        </div>
+        {/* header right */}
+<div>
+
+</div>
+
+    </div>
+  )
+}
+
+export default Header   
